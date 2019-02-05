f6fcf102f974cd98fa9b7e6be16e5cf1127c296c
Melody Horn <melody@boringcactus.com>
2019-02-05 15:59:33 -0700

    tweak editor layout to handle scrolling halfway reasonably

--- editor/assets/index.js
+++ editor/assets/index.js
@@ -3,3 +3,12 @@ import Editor from './Editor.html';
 const app = new Editor({
 	target: document.querySelector('main'),
 });
+
+function fixPageWidth() {
+	document.documentElement.style.setProperty('--page-width', document.documentElement.clientWidth + 'px');
+}
+
+setTimeout(fixPageWidth, 1);
+
+document.addEventListener('resize', fixPageWidth);
+document.addEventListener('domContentLoaded', fixPageWidth);
