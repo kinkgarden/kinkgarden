9e62e94feb764f6a3d7773065beffe6420838b3e
Melody Horn <melody@boringcactus.com>
2019-03-09 11:36:22 -0700

    clean up formatting

--- editor/assets/index.js
+++ editor/assets/index.js
@@ -1,11 +1,11 @@
 import Editor from './Editor.html';
 
 const app = new Editor({
-	target: document.querySelector('main'),
+    target: document.querySelector('main'),
 });
 
 function fixPageWidth() {
-	document.documentElement.style.setProperty('--page-width', document.documentElement.clientWidth + 'px');
+    document.documentElement.style.setProperty('--page-width', document.documentElement.clientWidth + 'px');
 }
 
 setTimeout(fixPageWidth, 1);
