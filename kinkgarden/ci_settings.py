012d72cb3a621511bec6d450bf0e52751866921c
Melody Horn <melody@boringcactus.com>
2020-08-11 23:46:48 -0600

    fix travis setup, attempt 1

---
+++ kinkgarden/ci_settings.py
@@ -0,0 +1,10 @@
+from .settings import *
+
+SECRET_KEY = 'insecure'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
